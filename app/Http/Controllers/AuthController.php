995243a8f553bc5f3d31043d0ba0eb51c9959bb6
zooma123 <93126188+zooma123@users.noreply.github.com>
2025-06-13 17:43:04 +0300

    Make Function For SignUp

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SignUpRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+public function Register(SignUpRequest $request){
+
+$validated = $request->validated();
+
+$validated['password'] = Hash::make($request->password);
+
+User::create($validated);
+
+response()->json([
+'message' => "تم انشاء الحساب بنجاح "
+
+]);
+
+}
+
+
+
+}
---
+++ app/Http/Requests/SignUpRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SignUpRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+        
+"name" => 'required|string|max:200' ,
+"email" => 'required|email|string|max:200',
+"password" => "required|string|min:6|confirmed",
+"role" => "required" ,
+[
+'name.required' => 'يجب ادخال اسمك الحقيقي' ,
+'email.required' => 'يحب ادخال ايميل صحيح' , 
+'password.required' => 'كلمة المرور مطلوبة.',
+'password.min' => 'كلمة المرور يجب أن تكون على الأقل 6 أحرف.',
+'password.confirmed' => 'الباسورد غير مطابق احا'
+]
+
+
+        ];
+    }
+}
